eb4204b469c39d3f0ef8d4ff6568f1756ec6d98e
elorie <elorie@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-05-27 11:59:30 +0000

    mods
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@16 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Django/ClickDummyNM/ClickDummyNM/views.py
+++ Django/ClickDummyNM/ClickDummyNM/views.py
@@ -7,20 +7,19 @@ from django.shortcuts import render_to_response
 #from django.template.loader import get_template
 
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {'content_text': "Welcome to NewsMiner+", 
                     'dossier':"/dossier/"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render_to_response('index.html', context_dict, context)
 
 def dossier(request):
+    context = RequestContext(request)
+    context_dict = {'dossier_title': "Welcome to NewsMiner+", 
+                    'article_text':"/dossier/",
+                    'widget_1': "WIKI-Bild",
+                    'widget_2': "WIKI-Box",
+                    'widget_3': "Google-Maps",
+                    }
+    return render_to_response('index.html', context_dict, context)
     return HttpResponse("<b>Hello Dossier!</b><br><a href='/'>Home</a>")
 
