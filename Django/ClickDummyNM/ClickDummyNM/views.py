972a246398d815ef3e42c631bb886a797de1ff2c
elorie <elorie@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-05-27 11:45:18 +0000

    mods
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@9 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Django/ClickDummyNM/ClickDummyNM/views.py
+++ Django/ClickDummyNM/ClickDummyNM/views.py
@@ -1,9 +1,24 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 #from django.template import Template, Context
 #from django.template.loader import get_template
 
 def index(request):
-    return render_to_response('index.html')
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'content_text': "<h1>Welcome to NewsMiner+</h1><br /><ul><li><a href='/dossier/'>Dossier</a></li></li>"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('index.html', context_dict, context)
 
 def dossier(request):
     return HttpResponse("<b>Hello Dossier!</b><br><a href='/'>Home</a>")
