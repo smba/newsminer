bf7458e5eab19fb863be74e9d7ac02b13ab243d4
elorie <elorie@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-05-27 11:53:22 +0000

    mods
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@13 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Django/ClickDummyNM/ClickDummyNM/views.py
+++ Django/ClickDummyNM/ClickDummyNM/views.py
@@ -13,7 +13,8 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'content_text': "Welcome to NewsMiner+", 'dossier':'"/dossier/"}
+    context_dict = {'content_text': "Welcome to NewsMiner+", 
+                    'dossier':"/dossier/"}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
