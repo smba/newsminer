191732fb6482da1700031f7950b244b363628c0c
sushimitzu <sushimitzu@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-06-25 17:05:53 +0000

    entity_persons
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@61 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- SQL/CREATE TABLE/entity_persons.sql
+++ SQL/CREATE TABLE/entity_persons.sql
@@ -2,5 +2,9 @@ CREATE TABLE entity_persons
 (
   name text NOT NULL,
   description text,
+  date_of_birth date,
+  notable_for text,
+  place_of_birth text,
+  image text,
   CONSTRAINT entity_locations_pkey PRIMARY KEY (name)
 );
