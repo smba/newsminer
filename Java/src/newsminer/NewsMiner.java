8f5f19de1b466ac1c41f0c84886f290b6edda3e1
sushimitzu <sushimitzu@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-07-01 11:15:10 +0000

    SSH removed
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@69 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Java/src/newsminer/NewsMiner.java
+++ Java/src/newsminer/NewsMiner.java
@@ -1,6 +1,5 @@
 package newsminer;
 
-import java.io.Console;
 import java.io.IOException;
 
 import newsminer.rss.ArticleClusterer;
@@ -11,41 +10,17 @@ import newsminer.util.DatabaseUtils;
  * Coordinates the News Miner components.
  * 
  * @author  Timo Guenther
- * @version 2014-06-27
+ * @version 2014-07-01
  */
 public abstract class NewsMiner {
   /**
    * Launches the application.
-   * Will ask for SSH credentials if not already provided as arguments.
-   * @param args 0: SSH user name (optional);
-   *             1: SSH password (optional)
+   * @param args unused
    */
   public static void main(String[] args) {
-    //Get the SSH credentials.
-    final String sshUser, sshPassword;
-    if (args.length >= 2) {
-      sshUser     = args[0];
-      sshPassword = args[1];
-    } else {
-      final Console console = System.console();
-      if (console == null) {
-        throw new IllegalArgumentException("Missing SSH credentials");
-      }
-      System.out.println("SSH user name?");
-      sshUser     = new String(console.readPassword());
-      System.out.println("SSH password?");
-      sshPassword = new String(console.readPassword());
-    }
-    
-    //Connect to the database.
-    try {
-      DatabaseUtils.getConnection(sshUser, sshPassword);
-    } catch (IOException ioe) {
-      throw new RuntimeException(ioe);
-    }
-    
     //Initialize the components.
     System.out.println("Initializing.");
+    DatabaseUtils.getConnection();
     final RSSCrawler       rssCrawler;
     final ArticleClusterer articleClusterer;
     try {
--- Java/src/newsminer/util/DatabaseUtils.java
+++ Java/src/newsminer/util/DatabaseUtils.java
@@ -12,16 +12,12 @@ import java.util.Properties;
  * Interacts with the database.
  * 
  * @author  Timo Guenther
- * @version 2014-06-27
+ * @version 2014-07-01
  */
 public abstract class DatabaseUtils {
   //constants
   /** path to the properties file for connecting to the database */
   private static final String CONNECTION_PROPERTIES_FILE_PATH = "conf/database_connection.properties";
-  /** path to the properties file for connecting to the SSH tunnel */
-  //TODO private static final String SSH_PROPERTIES_FILE_PATH        = "conf/ssh.properties";
-  /** default SSH port */
-  //TODO private static final int    SSH_PORT                        = 22;
   
   //attributes
   /** connection to the database */
@@ -32,60 +28,32 @@ public abstract class DatabaseUtils {
    * @return the established database connection
    */
   public synchronized static Connection getConnection() {
-    //Check if the connection has been established yet.
-    if (connection == null) {
-      throw new IllegalStateException();
-    }
-    
-    //Return the connection.
-    return connection;
-  }
-  
-  /**
-   * Returns the established database connection using the given SSH credentials.
-   * @param  sshUser user name for the SSH tunnel
-   * @param  sshPass password for the SSH tunnel
-   * @return the established database connection
-   * @throws SQLException
-   */
-  public synchronized static Connection getConnection(String sshUser, String sshPass) throws IOException {
-    //Establish the connection if it is not already established.
+    //Establish the connection if necessary.
     if (connection == null) {
       //Check for the PostgreSQL JDBC driver.
       try {
         Class.forName("org.postgresql.Driver");
       } catch (ClassNotFoundException cnfe) { //not installed
-        throw new IOException(cnfe);
+        throw new RuntimeException(cnfe);
       }
       
       //Get the database connection properties.
       final Properties properties = new Properties();
       try (final InputStream in = new FileInputStream(CONNECTION_PROPERTIES_FILE_PATH)) {
         properties.load(in);
+      } catch (IOException ioe) {
+        throw new RuntimeException(ioe);
       }
-      
-      //Access the SSH tunnel.
-      /** TODO fix
-      try {
-        final Session session = new JSch().getSession(
-            properties.getProperty("user"),
-            properties.getProperty("host"),
-            SSH_PORT);
-        session.setPassword(properties.getProperty("password"));
-        session.setConfig(FileUtils.getProperties(SSH_PROPERTIES_FILE_PATH));
-        session.connect();
-      } catch (JSchException jsche) {
-        throw new IOException(jsche);
-      }
-      */
       
       //Establish the database connection.
-      final String url = String.format("jdbc:postgresql://localhost:%s/%s",
-          properties.getProperty("port"), properties.getProperty("name"));
+      final String url = String.format("jdbc:postgresql://%s:%s/%s",
+          properties.getProperty("host"),
+          properties.getProperty("port"),
+          properties.getProperty("name"));
       try {
         connection = DriverManager.getConnection(url, properties);
-      } catch (SQLException ioe) {
-        throw new IOException(ioe);
+      } catch (SQLException sqle) {
+        throw new RuntimeException(sqle);
       }
     }
     
