01dda6bbb6999d7f32b9faa1809d5b23a0888d92
elorie <elorie@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-07-11 15:27:40 +0000

    donnerstag
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@84 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Django/ClickDummyNM/ClickDummyNM/views.py
+++ Django/ClickDummyNM/ClickDummyNM/views.py
@@ -145,9 +145,7 @@ def dossier(request, cluster_id):
     specific_context_dict = {
                     'dossier_title': "Welcome to Dossier No " + str(cluster_id), 
                     'article_text':"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Option congue nihil imperdiet doming id quod mazim placerat facer",
-                    'widget_1': "WIKI-Bild",
-                    'widget_2': "WIKI-Box",
-                    'widget_3': "Google-Maps",
+                    'widgets': ["WIKI-Bild", "WIKI-Box", "Google-Maps"],
                     'articles': articles,
                     'locations': locations,
                     'map_center':map_center,
