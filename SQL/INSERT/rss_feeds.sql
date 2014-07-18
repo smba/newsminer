40917ce9a4548205257869091ade1e0979abc38f
sushimitzu <sushimitzu@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-07-18 09:48:03 +0000

    country
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@105 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- SQL/INSERT/rss_feeds.sql
+++ SQL/INSERT/rss_feeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO rss_feeds VALUES
-  ('http://america.aljazeera.com/content/ajam/articles.rss',                'Al Jazeera: America'),
-  ('http://feeds.bbci.co.uk/news/rss.xml',                                  'BBC: World'),
-  ('http://www.theguardian.com/world/rss',                                  'The Guardian: World'),
-  ('http://www.huffingtonpost.co.uk/feeds/verticals/uk-politics/index.xml', 'Huffington Post: Politics'),
-  ('http://mondediplo.com/backend',                                         'Le Monde: Diplomatique'),
-  ('http://en.ria.ru/export/rss2/world/index.xml',                          'Ria Novosti: World'),
-  ('http://www.spiegel.de/international/index.rss',                         'Spiegel Online: International');
+INSERT INTO rss_feeds(source_url, name, country) VALUES
+  ('http://america.aljazeera.com/content/ajam/articles.rss',                'Al Jazeera: America',           'qa'),
+  ('http://feeds.bbci.co.uk/news/rss.xml',                                  'BBC: World',                    'uk'),
+  ('http://www.theguardian.com/world/rss',                                  'The Guardian: World',           'uk'),
+  ('http://www.huffingtonpost.co.uk/feeds/verticals/uk-politics/index.xml', 'Huffington Post: Politics',     'uk'),
+  ('http://mondediplo.com/backend',                                         'Le Monde: Diplomatique',        'fr'),
+  ('http://en.ria.ru/export/rss2/world/index.xml',                          'Ria Novosti: World',            'ru'),
+  ('http://www.spiegel.de/international/index.rss',                         'Spiegel Online: International', 'de');
