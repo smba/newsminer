3f6f1c27e14469bd45943c888edc6e9f698f29ca
sushimitzu <sushimitzu@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-07-18 09:53:37 +0000

    feed_id
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@106 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- SQL/CREATE TABLE/rss_articles.sql
+++ SQL/CREATE TABLE/rss_articles.sql
@@ -2,7 +2,7 @@ CREATE TABLE rss_articles
 (
   id serial NOT NULL,
   link text NOT NULL,
-  source_url text NOT NULL,
+  feed_id integer NOT NULL,
   "timestamp" bigint NOT NULL,
   title text NOT NULL,
   description text NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE rss_articles
   entity_organizations text[] NOT NULL DEFAULT '{}'::text[],
   entity_persons text[] NOT NULL DEFAULT '{}'::text[],
   CONSTRAINT rss_articles_pkey PRIMARY KEY (id),
-  CONSTRAINT rss_articles_source_url_fkey FOREIGN KEY (source_url)
-    REFERENCES rss_feeds (source_url) MATCH SIMPLE
-    ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT rss_articles_feed_id_fkey FOREIGN KEY (feed_id)
+    REFERENCES rss_feeds (id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
