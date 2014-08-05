6804f24249d8e13a6231787dbcbfd8d3355f248e
smba <smba@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-08-05 15:50:06 +0000

    add script
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@125 da7da56a-5241-427f-a2d7-db4bafe74fe4

---
+++ Java/start.py
@@ -0,0 +1,11 @@
+import sched, time
+import os
+
+s = sched.scheduler(time.time, time.sleep)
+TIMESTEP = 60*60*6 #every 6 hours
+def startNewsminer(sc):
+        print "Starting newsminer"
+        os.system("nohup java -jar newsminer.jar foo bar &")
+        sc.enter(TIMESTEP, 1, startNewsMiner, (sc,1))
+s.enter(TIMESTEP, 1, startNewsMine, (sc,))
+s.run()
