b07368791ed9c694d6269dead9d7588b38b96deb
parrotpaul <parrotpaul@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-08-05 15:57:06 +0000

    typo fixed
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@127 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Java/start.py
+++ Java/start.py
@@ -3,7 +3,7 @@ import os
 
 s = sched.scheduler(time.time, time.sleep)
 TIMESTEP = 60*60*6 #every 6 hours
-def startNewsminer(sc):
+def startNewsMiner(sc):
         print "Starting newsminer"
         os.system("nohup java -jar newsminer.jar foo bar &")
         sc.enter(TIMESTEP, 1, startNewsMiner, (sc,1))
