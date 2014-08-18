94cc76d8a0e19db1e7ab8415c2861071157bf231
parrotpaul <parrotpaul@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-08-18 11:17:35 +0000

    script problem
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@187 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- Django/newsminer2/newsminer2/settings.py
+++ Django/newsminer2/newsminer2/settings.py
@@ -21,6 +21,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
 
+STATIC_ROOT = "/var/www/newsminer/nm2static/"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -46,7 +48,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'dbarray',
     'leaflet',
-    #'dh5bp',
     'newsminer2',
 )
 
