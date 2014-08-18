9ca7a82b9b1105d1c3f8343765d3d68bc5e85392
smba <smba@da7da56a-5241-427f-a2d7-db4bafe74fe4>
2014-08-18 13:12:45 +0000

    updated feedlist
    git-svn-id: svn+ssh://svn.code.sf.net/p/newsminer/newsminer+@191 da7da56a-5241-427f-a2d7-db4bafe74fe4

--- SQL/INSERT/rss_feeds.sql
+++ SQL/INSERT/rss_feeds.sql
@@ -1,8 +1,14 @@
 INSERT INTO rss_feeds VALUES
-  ('http://america.aljazeera.com/content/ajam/articles.rss',                'Al Jazeera: America',           'qa'),
-  ('http://feeds.bbci.co.uk/news/rss.xml',                                  'BBC: World',                    'uk'),
-  ('http://www.theguardian.com/world/rss',                                  'The Guardian: World',           'uk'),
-  ('http://www.huffingtonpost.co.uk/feeds/verticals/uk-politics/index.xml', 'Huffington Post: Politics',     'uk'),
-  ('http://mondediplo.com/backend',                                         'Le Monde: Diplomatique',        'fr'),
-  ('http://en.ria.ru/export/rss2/world/index.xml',                          'Ria Novosti: World',            'ru'),
-  ('http://www.spiegel.de/international/index.rss',                         'Spiegel Online: International', 'de');
+  ('http://rss.nytimes.com/services/xml/rss/nyt/World.xml',          'New York Times',      'us'),
+  ('http://www.buenosairesherald.com/articles/rss.aspx',             'Buenos Aires Herald', 'ar'),
+  ('https://www.irishtimes.com/cmlink/news-1.1319192',               'Irish Times',         'ie'),
+  ('http://rss.feedsportal.com/c/266/f/3503/index.rss',              'The Independent',     'gb'),
+  ('http://www.theguardian.com/world/rss',                           'The Guardian',        'gb'),
+  ('http://mondediplo.com/backend',                                  'Le Monde',            'fr'),
+  ('http://www.spiegel.de/international/index.rss',                  'Spiegel Online',      'de'),
+  ('http://america.aljazeera.com/content/ajam/articles.rss',         'Al Jazeera',          'qa'),
+  ('http://www.kyivpost.com/rss/',                                   'Kyiv Post',           'ua'),
+  ('http://feeds.feedburner.com/InternationalRss',                   'Haaretz',             'il'),
+  ('http://www.hurriyetdailynews.com/rss.aspx',                      'Hurriyet',            'tr'),
+  ('http://en.ria.ru/export/rss2/world/index.xml',                   'Ria Novosti',         'ru'),
+  ('http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss', 'Times of India',      'in')
